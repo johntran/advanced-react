3a941bd347c738c53e3f1ecad3151e3bc6b3e2ed
Michael Jackson <mjijackson@gmail.com>
2015-09-02 11:42:23 -0700

    Update render-props lecture

--- subjects/02-render-props/lecture.js
+++ subjects/02-render-props/lecture.js
@@ -5,67 +5,62 @@ import searchGithubRepos from './lib/searchGithubRepos'
 import ScrollBottomNotifier from './lib/ScrollBottomNotifier'
 
 ////////////////////////////////////////////////////////////////////////////////
-// Here's a fun app, check out ScrollBottomNotifier, making these kinds of tasks declarative
-// is fantastic and makes code reuse really straightforward.
+// Here's a fun app, check out ScrollBottomNotifier, making these kinds of tasks
+// declarative is fantastic and makes code reuse really straightforward.
 
-//var App = React.createClass({
+var App = React.createClass({
 
-  //getInitialState () {
-    //return {
-      //repos: [],
-      //links: {},
-      //fetching: false
-    //};
-  //},
+  getInitialState () {
+    return {
+      repos: [],
+      links: {},
+      fetching: false
+    };
+  },
 
-  //fetch (url) {
-    //var defaultUrl = `https://api.github.com/search/repositories?q=react&sort=stars`;
-    //this.setState({ fetching: true });
-    //searchGithubRepos(url || defaultUrl, (err, repos, links) => {
-      //this.setState({
-        //repos: this.state.repos.concat(repos),
-        //links,
-        //fetching: false
-      //});
-    //});
-  //},
+  fetch (url) {
+    var defaultUrl = `https://api.github.com/search/repositories?q=react&sort=stars`;
+    this.setState({ fetching: true });
+    searchGithubRepos(url || defaultUrl, (err, repos, links) => {
+      this.setState({
+        repos: this.state.repos.concat(repos),
+        links,
+        fetching: false
+      });
+    });
+  },
 
-  //componentDidMount () {
-    //this.fetch()
-  //},
+  componentDidMount () {
+    this.fetch()
+  },
 
-  //fetchNextPage () {
-    //this.fetch(this.state.links.next);
-  //},
+  fetchNextPage () {
+    this.fetch(this.state.links.next);
+  },
 
-  //render () {
-    //var canFetch = !this.state.fetching && this.state.links.next;
-    //return (
-      //<div>
-        //<h1>Show her show her how you scroll, drop it drop it drop it low</h1>
-        //<h2>drop it, drop drop it low</h2>
+  render () {
+    var canFetch = !this.state.fetching && this.state.links.next;
+    return (
+      <ScrollBottomNotifier
+        style={styles.repos}
+        onScrollBottom={canFetch ? this.fetchNextPage : null}
+      >
+        <ul style={styles.repoList}>
+          {this.state.repos.map(repo => (
+            <li key={repo.id}>
+              <a href={repo.html_url} style={styles.repo}>
+                <h3>{repo.full_name}</h3>
+                <p>{repo.description}</p>
+              </a>
+            </li>
+          ))}
+        </ul>
+      </ScrollBottomNotifier>
+    );
+  }
+});
 
-        //<ScrollBottomNotifier
-          //style={styles.repos}
-          //onScrollBottom={canFetch ? this.fetchNextPage : null}
-        //>
-          //<ul style={styles.repoList}>
-            //{this.state.repos.map(repo => (
-              //<li key={repo.id}>
-                //<a href={repo.html_url} style={styles.repo}>
-                  //<h3>{repo.full_name}</h3>
-                  //<p>{repo.description}</p>
-                //</a>
-              //</li>
-            //))}
-          //</ul>
-        //</ScrollBottomNotifier>
-      //</div>
-    //);
-  //}
-//});
-
-//render(<App/>, document.getElementById('app'));
+render(<App/>, document.getElementById('app'));
 
 ////////////////////////////////////////////////////////////////////////////////
 // Making something like Scrolling a component makes a lot of sense, but what
@@ -113,15 +108,11 @@ import ScrollBottomNotifier from './lib/ScrollBottomNotifier'
 
   //render () {
     //var url = `https://api.github.com/search/repositories?q=react&sort=stars`;
-    ////var canFetch = !this.state.fetching && this.state.links.next;
+    //var canFetch = !this.state.fetching && this.state.links.next;
     //return (
       //<div>
-        //<h1>Show her show her how you scroll, drop it drop it drop it low</h1>
-        //<h2>drop it, drop drop it low</h2>
-
         //<GithubSearch url={url}/>
 
-        //{[>
         //<ScrollBottomNotifier
           //style={styles.repos}
           //onScrollBottom={canFetch ? this.fetchNextPage : null}
@@ -137,7 +128,6 @@ import ScrollBottomNotifier from './lib/ScrollBottomNotifier'
             //))}
           //</ul>
         //</ScrollBottomNotifier>
-        //*/}
       //</div>
     //);
   //}
@@ -151,84 +141,81 @@ import ScrollBottomNotifier from './lib/ScrollBottomNotifier'
 // a component to another? We send a callback, turns out we use that callback
 // for rendering too
 
-var GithubSearch = React.createClass({
+//var GithubSearch = React.createClass({
 
-  getInitialState () {
-    return {
-      repos: [],
-      links: {},
-      fetching: false
-    };
-  },
+  //getInitialState () {
+    //return {
+      //repos: [],
+      //links: {},
+      //fetching: false
+    //};
+  //},
 
-  fetch (url) {
-    this.setState({ fetching: true });
-    searchGithubRepos(this.props.url, (err, repos, links) => {
-      this.setState({
-        repos: this.state.repos.concat(repos),
-        links,
-        fetching: false
-      });
-    });
-  },
+  //fetch (url) {
+    //this.setState({ fetching: true });
+    //searchGithubRepos(this.props.url, (err, repos, links) => {
+      //this.setState({
+        //repos: this.state.repos.concat(repos),
+        //links,
+        //fetching: false
+      //});
+    //});
+  //},
 
-  componentDidMount () {
-    this.fetch();
-  },
+  //componentDidMount () {
+    //this.fetch();
+  //},
 
-  componentDidUpdate (prevProps) {
-    if (prevProps.url !== this.props.url)
-      this.fetch();
-  },
+  //componentDidUpdate (prevProps) {
+    //if (prevProps.url !== this.props.url)
+      //this.fetch();
+  //},
 
-  render () {
-    return this.props.children(this.state);
-  }
+  //render () {
+    //return this.props.children(this.state);
+  //}
 
-});
+//});
 
-var App = React.createClass({
+//var App = React.createClass({
 
-  getInitialState () {
-    return {
-      url: 'https://api.github.com/search/repositories?q=react&sort=stars'
-    };
-  },
+  //getInitialState () {
+    //return {
+      //url: 'https://api.github.com/search/repositories?q=react&sort=stars'
+    //};
+  //},
 
-  fetchNextPage (search) {
-    if (!search.fetching && search.links.next)
-      this.setState({ url: search.links.next});
-  },
+  //fetchNextPage (search) {
+    //if (!search.fetching && search.links.next)
+      //this.setState({ url: search.links.next});
+  //},
 
-  render () {
-    return (
-      <div>
-        <h1>Show her show her how you scroll, drop it drop it drop it low</h1>
-        <h2>drop it, drop drop it low</h2>
+  //render () {
+    //return (
+      //<div>
+        //<GithubSearch url={this.state.url}>
+          //{(search) => (
+            //<ScrollBottomNotifier
+              //style={styles.repos}
+              //onScrollBottom={() => this.fetchNextPage(search)}
+            //>
+              //<ul style={styles.repoList}>
+                //{search.repos.map(repo => (
+                  //<li key={repo.id}>
+                    //<a href={repo.html_url} style={styles.repo}>
+                      //<h3>{repo.full_name}</h3>
+                      //<p>{repo.description}</p>
+                    //</a>
+                  //</li>
+                //))}
+              //</ul>
+            //</ScrollBottomNotifier>
+          //)}
+        //</GithubSearch>
+      //</div>
+    //);
+  //}
+//});
 
-        <GithubSearch url={this.state.url}>
-          {(search) => (
-            <ScrollBottomNotifier
-              style={styles.repos}
-              onScrollBottom={() => this.fetchNextPage(search)}
-            >
-              <ul style={styles.repoList}>
-                {search.repos.map(repo => (
-                  <li key={repo.id}>
-                    <a href={repo.html_url} style={styles.repo}>
-                      <h3>{repo.full_name}</h3>
-                      <p>{repo.description}</p>
-                    </a>
-                  </li>
-                ))}
-              </ul>
-            </ScrollBottomNotifier>
-          )}
-        </GithubSearch>
-      </div>
-    );
-  }
-});
-
-render(<App/>, document.getElementById('app'));
+//render(<App/>, document.getElementById('app'));
 
