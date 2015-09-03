312f6b0be4052d13efcf1aa6eb3d98efd9e634f9
Ryan Florence <rpflorence@gmail.com>
2015-09-03 10:04:51 -0700

    perf

---
+++ subjects/06-react-perf/exercise.js
@@ -0,0 +1,88 @@
+import React from 'react'
+import { render } from 'react-dom'
+import Perf from 'react-addons-perf'
+
+let guid = 0
+
+let ITEMS = [];
+for (let i = 0; i < 2000; i++)
+  ITEMS.push({ id: ++guid, body: `item ${guid}` })
+
+class TodoItem extends React.Component {
+  render () {
+    return (
+      <li>
+        <span><input type="checkbox"/> </span>
+        <span>
+          <span><b>item:</b> </span>
+          <span className="stuff"/>
+          <span className="stuff"/>
+          <span className="stuff"/>
+          <span className="stuff"/>
+          <span className="stuff"/>
+          <span className="stuff"/>
+          <span className="stuff"/>
+          <span className="stuff"/>
+          <span className="stuff"/>
+        </span>
+        <span tabIndex="-1">
+          <span className="thing">
+            <span style={{color: 'blue'}}>{this.props.body}</span>
+          </span>
+        </span>
+      </li>
+    )
+  }
+}
+
+class TodoList extends React.Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      items: ITEMS
+    }
+  }
+
+  handleSubmit (e) {
+    e.preventDefault()
+    let item = {
+      id: ++guid,
+      body: e.target.elements[0].value
+    }
+    e.target.reset()
+
+    this.setState({
+      items: [item].concat(this.state.items)
+    })
+  }
+
+  render () {
+    return (
+      <div>
+        <form onSubmit={(event) => this.handleSubmit(event)}>
+          <input ref="input"/>
+        </form>
+        <ul>
+          {this.state.items.map(item => (
+            <TodoItem key={item.id} body={item.body}/>
+          ))}
+        </ul>
+      </div>
+    )
+  }
+}
+
+class App extends React.Component {
+  render () {
+    return (
+      <div>
+        <h1>Perf</h1>
+        <TodoList/>
+      </div>
+    )
+  }
+}
+
+render(<App/>, document.getElementById('app'))
+
